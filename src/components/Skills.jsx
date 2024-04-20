11ff1357e1576a78a88dedbf99c844eaf763a0fe
sdy329 <sdy329@gmail.com>
2024-04-20 02:15:43 -0400

    Made skills rely on db

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,15 @@
-import skillArray from "@/components/SkillArray"
+import { createClient } from "@/utils/supabase/server";
 
-export default function Skills() {
+export default async function Skills() {
+    const supabase = createClient();
+    const { data: skills, error } = await supabase
+        .from('skills')
+        .select()
+        .order('id', { ascending: true });
 
-    const skillsArray = skillArray();
+    if (error) {
+        return { error };
+    }
 
     return (
         <section id="skills" className="lg:mb-20 mb-10">
@@ -11,14 +18,14 @@ export default function Skills() {
             </div>
             <div className="container mx-auto">
                 <div className="flex flex-wrap sm:mx-auto sm:mb-2">
-                    {skillsArray.map((skill) => (
+                    {skills.map((skill) => (
                         <div key={skill.id} className="p-2 sm:w-1/2 w-full">
                             <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="text-cyan-600 h-10 w-10 mr-4" viewBox="0 0 500 500">
-                                    <path d={skill.path} />
+                                    <path d={skill.svg} />
                                 </svg>
                                 <span className="title-font font-medium text-white">
-                                    {skill.title}
+                                    {skill.name}
                                 </span>
                             </div>
                         </div>
