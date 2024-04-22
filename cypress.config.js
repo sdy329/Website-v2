f5b50ca5158d104c3ac3cf0d25f481b844cbcbc0
sdy329 <sdy329@gmail.com>
2024-04-22 14:03:48 -0400

    Removed useless code

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
-const path = require('path')
 
 module.exports = defineConfig({
   projectId: 'gardwe',
   e2e: {
     baseUrl: 'http://localhost:3000',
-    env: {
-    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
