65c3512a27e476e627f90d5557bf34dcc62def59
sdy329 <sdy329@gmail.com>
2024-05-23 11:49:51 -0400

    Fixed function name to appropriate name Resume()

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default async function Discord() {
+export default async function Resume() {
     return redirect("https://kxlddvjdxrsutvfwvrzr.supabase.co/storage/v1/object/public/personal-files/SYates-Resume.pdf");
 }
