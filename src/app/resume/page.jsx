34c88c2b4657df42b619d166df91f4a5d94e1fb0
sdy329 <sdy329@gmail.com>
2024-05-23 11:25:10 -0400

    Updated version and added /resume

---
+++ src/app/resume/page.jsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default async function Discord() {
+    return redirect("https://kxlddvjdxrsutvfwvrzr.supabase.co/storage/v1/object/public/personal-files/SYates-Resume.pdf");
+}
